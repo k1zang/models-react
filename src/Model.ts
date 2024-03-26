b871a5095d7713b1a91a645fd2434a14cc6af693
K1Zang <keivanpourzang@gmail.com>
2024-03-26 19:37:52 +0300

    Update import statement in Model.ts

--- src/Model.ts
+++ src/Model.ts
@@ -1,11 +1,7 @@
-import { Model as BaseModel } from "@k1/models";
+import { Model as BaseModel } from "@k1zang/models";
 import GraphqlManager from "./GraphqlManager";
 
 export default class Model extends BaseModel {
-  //   constructor(data) {
-  //     super(data);
-  //   }
-
   protected managers = {
     ...BaseModel.prototype.managers,
     graphql: GraphqlManager,
